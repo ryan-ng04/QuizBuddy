bec8406f764dc301128daadfcd1b138428d79b05
ryannoobng04 <ryanmhng04@gmail.com>
2024-10-03 02:36:42 -0500

    Added User Button

--- hp.js
+++ hp.js
@@ -1,6 +1,28 @@
-document.getElementById('sidebarToggle').addEventListener('click', function() {
+//handles username in the corner
+var usernameInput = document.getElementById("username");
+var saveButton = document.getElementById("saveButton");
+var greetingDisplay = document.getElementById("greetingDisplay");
+//button code
+
+saveButton.addEventListener('click', function () {
+    var username = usernameInput.value;
+    
+    if (username) {
+        var greetingMessage = (`Hello Welcome ${username}!`);
+        greetingDisplay.innerHTML = greetingMessage;
+        usernameInput.hidden = true;
+        saveButton.hidden = true;
+    }
+});
+
+
+
+
+//sidebar
+document.getElementById('sidebarToggle').addEventListener('click', function () {
     document.body.classList.toggle('sidebar-open');
 });
+//btn to go to index.html
 document.querySelector('.btn').addEventListener('click', function(event) {
     event.preventDefault();
     window.location.href = 'index.html';
